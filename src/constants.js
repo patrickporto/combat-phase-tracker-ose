7f779b449c530dfb98313698fd0a6d6c704fbd47
Patrick Porto <patrick.s.porto@gmail.com>
2023-12-02 10:18:03 -0700

    chore: setup project

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const CANONICAL_NAME = 'fvtt-module-template';
-export const MODULE_NAME = 'FVTT Module Template';
+export const CANONICAL_NAME = 'combat-phase-tracker-ose';
+export const MODULE_NAME = 'Combat Phase Tracker (Old School Essentials)';
 export const MODULE_PATH = `modules/${CANONICAL_NAME}`;
 export const TEMPLATE_PATH = `${MODULE_PATH}/templates`;
